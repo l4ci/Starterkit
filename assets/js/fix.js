d558e732eb82ec740ecef5eba0e53c52917a2952
Volker Otto <hello@volkerotto.net>
2014-11-11 09:58:24 +0100

    first commit

---
+++ assets/js/fix.js
@@ -0,0 +1,39 @@
+/**
+ * Avoid `console` errors in browsers that lack a console.
+ */
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+/**
+ * Windows Phone 8 and Device-Width FIX - JS PART
+ * Source: http://timkadlec.com/2013/01/windows-phone-8-and-device-width/
+ */
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:auto!important}"
+        )
+    );
+    document.getElementsByTagName("head")[0].
+        appendChild(msViewportStyle);
+}
