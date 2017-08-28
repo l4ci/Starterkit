042c5a8c802f75f8fc699dc4d6f3166f74024d24
Volker Otto <volker.otto@digital-masters.de>
2017-08-28 16:18:43 +0200

    added js boilerplate module

---
+++ src/js/ui/_boilerplate.js
@@ -0,0 +1,35 @@
+/**
+ * Example Boilerplate Module
+ *
+ * Add boilerplateModule.init() to main.js to initiate it.
+ */
+var boilerplateModule = (function(){
+
+    // Add variables and elements
+    var element = $('.some-element');
+
+    // Add functions
+    var doSomething = function(){
+        console.log('Boilerplate running.');
+    }
+
+    // Add event handlers and keybindings here
+    var setupBindings = function(){
+        element.on('click',function(e){
+            e.preventDefault();
+            doSomething();
+        });
+
+        // Always append module name on global functions
+        $(window).on('scroll.boilerplateModule', doSomething);
+    };
+
+    var init = function(){
+        setupBindings();
+    };
+
+    // Expose publicly
+    return {
+        init: init;
+    }
+});
