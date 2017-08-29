b1a4f6e5765626ecef85175210af2f3d56689819
Volker Otto <volker.otto@digital-masters.de>
2017-08-29 16:45:37 +0200

    better scroll-to function

--- src/js/ui/_data-scrollto.js
+++ src/js/ui/_data-scrollto.js
@@ -1,8 +1,18 @@
-$('[data-scrollto]').click(function() {
-    var target = $($(this).data('scrollto'));
+$('[data-scrollto]').on('click', function(e) {
+    var tHash = $(this).data('scrollto');
+    var target = $(tHash);
     if (target.length) {
+        var amount = $('.header').height();
         $('html,body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
+            scrollTop: target.offset().top - amount
+        }, 500);
+
+        if (history.pushState) {
+            history.pushState(null, null, tHash);
+        } else {
+            location.hash = tHash;
+        }
+        e.preventDefault();
+        return false;
     }
 });
