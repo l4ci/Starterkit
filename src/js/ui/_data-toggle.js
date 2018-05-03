8517fb1d3fef2503caad21f38a904861f13d19d6
Volker Otto <volker.otto@digital-masters.de>
2018-05-03 12:37:46 +0200

    added accordion to data-toggle

--- src/js/ui/_data-toggle.js
+++ src/js/ui/_data-toggle.js
@@ -1,16 +1,25 @@
 // General Toggle Function
 // Usage:
 // <div data-toggle="#toggle1"></div>
-// <el id="toggle1"></el>
+// <el id="toggle1" data-group="accordion"></el>
 $('[data-toggle]').on('click',function(e){
     e.preventDefault();
 
     var target = $($(this).data('toggle'));
     if (target.length) {
+
+        var group = target.data('group');
+        if (group) {
+            var groups = '[data-group="'+group+'"]';
+            $(groups).not(target)
+                .removeClass('--open')
+                .attr('aria-hidden', 'true');
+        }
+
         if (target.hasClass('--open')){
             target.removeClass('--open')
                   .attr('aria-hidden', 'true');
-        }else{
+        } else {
             target.addClass('--open')
                   .attr('aria-hidden', 'false');
         }
